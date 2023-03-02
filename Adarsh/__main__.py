9dd77087b67dee17b80517b39e352bc2dd823c44
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-03-03 01:51:49 +0530

    op

--- Adarsh/__main__.py
+++ Adarsh/__main__.py
@@ -66,7 +66,7 @@ async def start_services():
     print('\n')
     print('---------------------------------------------------------------------------------------------------------')
     print('---------------------------------------------------------------------------------------------------------')
-    print(' follow me for more such exciting bots! https://github.com/aadhi000')
+    print(' follow me for more such exciting bots! https://t.me/Opleech')
     print('---------------------------------------------------------------------------------------------------------')
     print('\n')
     print('----------------------- Service Started -----------------------------------------------------------------')
@@ -76,7 +76,7 @@ async def start_services():
     if Var.ON_HEROKU:
         print('                        app runnng on =>> {}'.format(Var.FQDN))
     print('---------------------------------------------------------------------------------------------------------')
-    print('Give a star to my repo https://github.com/adarsh-goel/filestreambot-pro  also follow me for new bots')
+    print('Give a lick to my bot https://t.me/WD_Filestream_bot  also follow me for new bots')
     print('---------------------------------------------------------------------------------------------------------')
     await idle()
 
